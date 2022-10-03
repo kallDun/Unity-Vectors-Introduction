1fdd0f277d4bb1f52fe6c1a9ef8971945d5e341f
kallDun <igorxlonline24@gmail.com>
2022-10-03 13:19:29 +0300

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Transform goal;
+    public float speed = 0.4f;
+    public float accuracy = 0.25f;
+
+    void Start() 
+    {
+        
+    }
+
+    void LateUpdate() 
+    {
+        transform.LookAt(goal.position);
+        Vector3 dir = goal.position - transform.position;
+        if (dir.magnitude < accuracy) return;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+    }
+}
